810d75ac4f87fcfed894d9537ed57406792e844f
Sean Oughton <seanoughton@gmail.com>
2017-12-28 17:18:13 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,9 +2,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+  
+const body = document.querySelector('body')
+body.addEventListener( 'keydown',function(){alert('')} )
 
 }
 
-
-const body = document.querySelector('body')
-body.addEventListener( 'click',function(){alert('')} )
