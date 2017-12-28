139d8a47abc737d51becc60b7ff939a3e131f769
Sean Oughton <seanoughton@gmail.com>
2017-12-28 17:29:24 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,12 +2,12 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-  function onKeyDownHandler(e){
+  function whenKeyDownOccurs(e){
      alert('')
   }
   
     const body = document.querySelector('body')
-    body.addEventListener('keydown',onKeyDownHandler)
+    body.addEventListener('keydown',whenKeyDownOccurs)
 
 }
 
